c0502d3fa901f023a39d0efa55977436f1de587d
MohammadNasir520 <nasirahsan520@gmail.com>
2024-05-29 01:29:15 +0600

    git correction

--- src/app/modules/tuitionPost/tuitionPost.routes.ts
+++ src/app/modules/tuitionPost/tuitionPost.routes.ts
@@ -2,10 +2,8 @@ import express from 'express';
 import { TuitionPostController } from './tuitionPost.controller';
 
 const router = express.Router();
-
 router.post('/', TuitionPostController.insertToDB);
 router.get('/', TuitionPostController.getAllFromDB);
-
 router.get('/:id', TuitionPostController.getSingleById);
 
 export const TuitionPostRouter = router;
